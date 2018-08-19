44ba38b73dc200a8b1753daa5cd93b283efec033
basiji <40441882+basiji@users.noreply.github.com>
2018-08-19 09:39:47 +0430

    crawler

--- crawler.js
+++ crawler.js
@@ -1,12 +1,16 @@
 var fs = require('fs');
 var parser = require('fast-html-parser');
 var request = require('request');
+var mysql = require('mysql');
+var CONSTANTS = require(__dirname + '/modules/constants.js');
+var connection = mysql.createConnection(CONSTANTS.MySQL);
 var dubways_all_url = 'https://www.radiojavan.com/podcasts/browse/show/Dubways';
-var dubways_95_url = 'https://www.radiojavan.com/podcasts/podcast/Dubways-95'
-/* Receive html from url 
-request(url, function (error, response, body){
-    console.log(response.statusCode);
-});*/
+
+// Establish MySQL connection
+connection.connect(function(error){
+    if(error)
+    console.log(error);
+});
 
 var stream  = request(dubways_all_url).pipe(fs.createWriteStream('temp.html'));
 stream.on('finish',function(){
@@ -14,13 +18,27 @@ stream.on('finish',function(){
     var root = parser.parse(document.toString());
     var blocks = root.querySelectorAll("a.block_container");
     var block;
+    var title, episode, thumbnail, link;
     for (var i = 0; i < blocks.length; i++) {
-    block = blocks[i];
-    console.log("Title : " + block.childNodes[3].childNodes[1].childNodes[0].rawText);
-    console.log("Episode : " + block.childNodes[3].childNodes[3].childNodes[0].rawText.split(' ')[1]);
-    console.log("Thumbnail : " + block.childNodes[1].childNodes[1].rawAttrs.split('"')[1]);
-    console.log("Podcast link : " + block.rawAttrs.split('"')[1]);
-    } 
+    
+        block = blocks[i];
+    // Get required information
+    title =  block.childNodes[3].childNodes[1].childNodes[0].rawText;
+    episode =  block.childNodes[3].childNodes[3].childNodes[0].rawText.split(' ')[1];
+    thumbnail =  block.childNodes[1].childNodes[1].rawAttrs.split('"')[1];
+    link = block.rawAttrs.split('"')[1];
+
+    // Insert into DB
+    connection.query("INSERT INTO app_podcasts SET ?", {
+        title:title,
+        episode:episode,
+        thumbnail_path:thumbnail,
+        link:link
+    }, function(error){
+        if(error)
+        console.log(error);
+    });
+} 
 });
 
 
