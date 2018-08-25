2a12fd7d400d95cdf85e0974d121479a00674dab
basiji <40441882+basiji@users.noreply.github.com>
2018-08-25 18:17:37 +0430

    x

--- test.js
+++ test.js
@@ -9,16 +9,22 @@ connection.connect(function(error){
 
 connection.query("SELECT * FROM app_podcasts ORDER BY id DESC", function(error, result){
 
-    for (var i = 0; i < result.length; i++) {
-        // Update download_path
-        connection.query("UPDATE app_podcasts SET download_path = '" + CONSTANTS.PS_PODCASTS_BASE + result[i].id + ".mp3' WHERE id = '" + result[i].id + "'", function(error){
+    if(error)
+    return console.log(error);
 
-            if(error)
-                console.log(error);
-            else 
-                console.log(result[i].title + '(' + result[i].episode + ') -> updated.');
-
+    for (var i = 0; i < podcasts.length; i++) {
+        
+        updater(result[i], function(message){
+            console.log(message);
         });
     }
-
 });
+
+function updater(podcast, callback){
+    connection.query("UPDATE app_podcasts SET download_path = '" + CONSTANTS.PS_PODCASTS_BASE + podcast.id + ".mp3' WHERE id = '" + podcast.id + "'", function(error){
+        if(error)
+        callback(error);
+        else
+        callback('success');
+    })
+}
