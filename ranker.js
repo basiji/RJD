279873dbefbd028bd872375d28099ce183163b88
basiji <40441882+basiji@users.noreply.github.com>
2018-08-25 12:58:41 +0430

    x

--- ranker.js
+++ ranker.js
@@ -12,21 +12,26 @@ connection.query("SELECT * FROM app_shows", function (error, result){
     for (var i = 0; i < result.length; i++){
 
         var show = result[i];
-        // Featured SQL
-        var sql_featured = "UPDATE app_podcasts SET featured = 1 WHERE id = (SELECT MAX(id) FROM app_podcasts WHERE showid = '" + show.id + "')";
-        // Popular SQL
-        var sql_popular = "UPDATE app_podcasts SET popular = 1 WHERE id = (SELECT id FROM app_podcasts WHERE showid = '" + show.id + "' ORDER BY likes DESC LIMIT 1)";
+        connection.query("SELECT * FROM app_podcasts WHERE showid = '" + show.id + "' ORDER BY episode DESC LIMIT 10", function(error, result){
 
-        connection.query(sql_featured, function(error){
-            if(error)
-            console.log(error);
+            var podcasts = result;
 
-            connection.query(sql_popular, function(error){
-                if(error)
-                console.log(error);
+            // Set last record as featured
+            connection.query("UPDATE app_podcasts SET featured = 1 WHERE id = '" + podcasts[0].id + "'", function(error){
+
+                // Order rows by likes
+                podcasts.sort(function(p1, p2) {return p2.likes - p1.likes;});
+
+                // Set most likes as popular
+                connection.query("UPDATE app_podcasts SET popular = 1 WHERE id = '" + podcasts[0].id + "'", function(error){
+                    if(error)
+                    console.log(error);
+                });
+
             });
-        });
 
+        });
+      
     }
 
 });
