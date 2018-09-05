bd384b034a4bfc4a456c46da29d8f2f817888e41
basiji <40441882+basiji@users.noreply.github.com>
2018-09-05 23:40:42 +0430

    x

--- index.js
+++ index.js
@@ -21,15 +21,7 @@ app.listen(CONSTANTS.PORT, function(){
 });
 
 // Serve static resources
-app.use(express.static(__dirname + '/html/ASAN'));
-
-// Static resources cache enabled
-app.use(function (req, res, next) {
-    if (req.url.match(/^\/(css|js|img|font)\/.+/)) {
-        res.setHeader('Cache-Control', 'public, max-age=3600'); 
-    }
-    next();
-});
+app.use(express.static(__dirname + '/html/ASAN', {maxAge:36000000}));
 
 // Router versioning
 app.use('/', router_v1);
